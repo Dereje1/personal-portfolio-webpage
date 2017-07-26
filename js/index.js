c21dbcf3537674f3f28df8ca6ac4ccf611212b40
Dereje Getahun <getahun.d@gmail.com>
2017-07-26 16:14:36 -0400

    carousel

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
   $(document).ready(function() {
+
+  });
+
     //scrolls to a specified section on the when a navigation link is clicked
     $(".nav-link").on("click", function(){
     //find the inner text of the navigation link clicked, this should have the same name
@@ -11,4 +14,29 @@
     }, 2000);
     
     });
-  });
+
+$('#carouselPortfolio').on('slide.bs.carousel', function (event) {
+  
+  var portfolioType = event.relatedTarget.id
+  switch (portfolioType){
+    case 'frontEndBasic':
+      $('#portfolioTitle').empty();
+      $('#portfolioTitle').append('Front End Portfolio');
+      $('.subPortfoilo').empty();
+      $('.subPortfoilo').append("Basic");
+      break;
+    case 'frontEndIntermediate':
+      $('#portfolioTitle').empty();
+      $('#portfolioTitle').append('Front End Portfolio');
+      $('.subPortfoilo').empty();
+      $('.subPortfoilo').append("Intermediate");
+      break;
+    case 'frontEndAdvanced':
+      $('#portfolioTitle').empty();
+      $('#portfolioTitle').append('Front End Portfolio');
+      $('.subPortfoilo').empty();
+      $('.subPortfoilo').append("Advanced");
+      break;
+   }
+  //console.log($('#carouselPortfolio').children()[0].firstElementChild)
+})
